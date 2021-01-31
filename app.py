853a96fc553f5654bddf370a29cfbb7d89bcceb4
SeongUk <pkseonguk@gmail.com>
2021-01-31 14:35:42 +0900

    Initialize home.html post.html from bootstrap template

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 80)
