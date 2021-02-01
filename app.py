ecdd6c16e43a781b11043d4a2b957c0a9490f3ca
SeongUk <pkseonguk@gmail.com>
2021-02-01 13:14:27 +0900

    Add post page parameter title

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request
+from flaskext.markdown import Markdown
 import frontmatter, os
 app = Flask(__name__)
+
+post_list = {}
 
 @app.route('/')
 def home():
     path_dir = 'static/post'
     file_list = os.listdir(path_dir)
-    post_list = []
+    post_list.clear()
     for file in file_list:
-        post_list.append(frontmatter.load(path_dir + '/' + file))
+        ft = frontmatter.load(path_dir + '/' + file)
+        post_list[(ft['title'])] = ft
     return render_template('home.html', post_list=post_list)
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<post_id>/')
+def post(post_id):
+    return render_template('post.html', post=post_list[post_id])
 
 if __name__ == '__main__':
+    Markdown(app, extensions=['nl2br', 'fenced_code'])
     app.run(host = '0.0.0.0', port = 80)
