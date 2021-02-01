9d5fd4eaf9f192c276a744280e42acf9ec579882
SeongUk <pkseonguk@gmail.com>
2021-02-01 13:46:48 +0900

    Add loadPost() to load post every time to load

--- app.py
+++ app.py
@@ -5,18 +5,22 @@ app = Flask(__name__)
 
 post_list = {}
 
-@app.route('/')
-def home():
+def loadPost():
     path_dir = 'static/post'
     file_list = os.listdir(path_dir)
     post_list.clear()
     for file in file_list:
         ft = frontmatter.load(path_dir + '/' + file)
         post_list[(ft['title'])] = ft
+
+@app.route('/')
+def home():
+    loadPost()
     return render_template('home.html', post_list=post_list)
 
 @app.route('/post/<post_id>/')
 def post(post_id):
+    loadPost()
     return render_template('post.html', post=post_list[post_id])
 
 if __name__ == '__main__':
