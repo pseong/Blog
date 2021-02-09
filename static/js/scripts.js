7427ed1cc33ca62d8f72240f8bf26e2b6b146f17
SeongUk <pkseonguk@gmail.com>
2021-02-09 03:48:11 +0000

    Add search enter key

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,20 +1,24 @@
-/*!
-    * Start Bootstrap - SB Admin v6.0.2 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    (function($) {
+function enter() {
+    if (window.event.keyCode == 13) {
+        search()
+    }
+}
+
+function search() {
+    var searchtxt = document.getElementById("search").value;
+    location.href="/search/"+searchtxt
+}
+
+(function($) {
     "use strict";
 
-    // Add active state to sidbar nav links
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+    var path = window.location.href;
         $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
             if (this.href === path) {
                 $(this).addClass("active");
             }
         });
 
-    // Toggle the side navigation
     $("#sidebarToggle").on("click", function(e) {
         e.preventDefault();
         $("body").toggleClass("sb-sidenav-toggled");
