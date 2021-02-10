70cc50ab0c2e0681dc20e0a433258ba7bbd69a69
SeongUk <pkseonguk@gmail.com>
2021-02-10 00:43:44 +0000

    Apply run.py for apache server

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import frontmatter, os
 from collections import Counter
 import markdown
 import logging
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -62,6 +63,7 @@ def home():
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
     return render_template('home.html', post_list=post_list, category_list=category_list, len=len,
+                           BeautifulSoup=BeautifulSoup, markdown=markdown,
                            posts=pagination_posts,
                            page=page,
                            per_page=per_page,
@@ -123,4 +125,4 @@ def internal_error(e):
 
 if __name__ == '__main__':
     disq = Disqus(app)
-    app.run(host = '0.0.0.0', port = 80)
+    app.run(host = '0.0.0.0', port = 7273)
